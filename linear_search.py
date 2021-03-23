9afb1eae5fff2e488375e3270bb92ecf930738b4
Mikhail Lepekhin <supersky2325@gmail.com>
2021-03-23 09:12:59 +0300

    small changes to the linear search

--- linear_search.py
+++ linear_search.py
@@ -11,11 +11,12 @@ def parse_hubs(raw_hubs):
 def get_relevant_documents(query, relevant_hubs=None):
     translator = str.maketrans('', '', string.punctuation)
     query = query.lower().translate(translator)
-    
-    all_documents = [pickle.load(open(f'clean_files/{filename}', 'rb')) for filename in listdir('clean_files')]
+    filenames = sorted(listdir('clean_files'))
+
+    all_documents = [pickle.load(open(f'clean_files/{filename}', 'rb')) for filename in filenames]
     relevant_documents = []
     
     for document in all_documents:
         if query in document['title'].lower():
             relevant_documents.append(document)
-    return relevant_documents
+    return relevant_documents, filenames
